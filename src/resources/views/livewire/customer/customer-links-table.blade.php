bd4e9b5fae6dbf67a0954e3ee104c6acd152877d
Aroldo Santos <dev.aroldosantos@gmail.com>
2023-06-23 17:01:09 -0300

    Customers updates livewire

--- src/resources/views/livewire/customer/customer-links-table.blade.php
+++ src/resources/views/livewire/customer/customer-links-table.blade.php
@@ -1,7 +1,3 @@
 <a href="{{route('customers.update', ['id' => $model->id])}}" title="Atualizar pessoa" class="border border-yellow-200 rounded mr-1 px-2 py-2 bg-yellow-100 text-center align-middle text-gray-600 hover:bg-yellow-400">
     <i class="fas fa-pen"></i>
 </a>
-|
-<a href="{{route('customers.update', ['id' => $model->id])}}" title="Inscrever em treinamento" class="border border-yellow-200 rounded mr-1 px-2 py-2 bg-yellow-100 text-center align-middle text-gray-600 hover:bg-yellow-400">
-    <i class="fas fa-user-graduate"></i>
-</a>
